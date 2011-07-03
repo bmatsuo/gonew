506fd23ad7ac7ff4700bb6c276930613987793b9
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-07-03 04:28:06 -0700

    comment edit.

--- gonew_main.go
+++ gonew_main.go
@@ -3,14 +3,15 @@ package main
 *  File: gonew_main.go
 *  Author: Bryan Matsuo [bmatsuo@soe.ucsc.edu] 
 *  Created: Sat Jul  2 19:17:53 PDT 2011
-*  Usage: gonew [options] */
+*  Usage: gonew [options]
+*/
 import (
-    "fmt"
-    "flag"
     "os"
-    "github.com/kr/pretty.go"
     //"io"
     //"log"
+    "fmt"
+    "flag"
+    "github.com/kr/pretty.go"
 )
 
 const (
