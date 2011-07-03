b6df9e49d20e210f39a2f9d4683d7936c86b1eec
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-07-02 19:23:16 -0700

    Initial commit. Does nothing. Tests nothing.

---
+++ gonew_main.go
@@ -0,0 +1,16 @@
+package main
+/* 
+*  File: gonew_main.go
+*  Author: Bryan Matsuo [bmatsuo@soe.ucsc.edu] 
+*  Created: Sat Jul  2 19:17:53 PDT 2011
+*  Usage: gonew [options] */
+import (
+    //"fmt"
+    //"flag"
+    //"os"
+    //"io"
+    //"log"
+)
+
+func main() {
+}
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+/* 
+*  File: main_test.go
+*  Author: Bryan Matsuo [bmatsuo@soe.ucsc.edu] 
+*  Created: Sat Jul  2 19:17:53 PDT 2011
+*  Usage: gotest
+*/
+import (
+    "testing"
+)
+
+func TestDummy(T *testing.T) { }
