23ab9abeaedd3ecbc78cd9002d95dd8ef373e90d
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-07-03 02:34:29 -0700

    Add a function to collect config info and write it out (unused).

--- config.go
+++ config.go
@@ -5,11 +5,14 @@ package main
 *  Created: Sat Jul  2 23:09:50 PDT 2011
 */
 import (
+    "os"
+    "fmt"
+    "bytes"
+    "bufio"
+    "path/filepath"
     //"goconf.googlecode.com/hg"    // This does not work for some reason.
     //"conf"
     "github.com/kless/goconfig/config"
-    "os"
-    "path/filepath"
 )
 
 var ConfigFilename = filepath.Join(os.Getenv("HOME"), ".gonewrc")
@@ -61,4 +64,84 @@ func ReadConfig() os.Error {
         AppConfig.Host = NilRepoHost
     }
     return nil
+}
+
+func MakeConfig() os.Error {
+    var (
+        c       = config.NewDefault()
+        scanner = bufio.NewReader(os.Stdin)
+        errScan os.Error
+        buff    []byte
+    )
+    fmt.Printf("Enter your name: ")
+    buff, _, errScan = scanner.ReadLine()
+    if errScan != nil {
+        return errScan
+    }
+    c.AddOption("variables", "name", string(bytes.TrimRight(buff, "\n")))
+    fmt.Printf("Enter your email address: ")
+    buff, _, errScan = scanner.ReadLine()
+    if errScan != nil {
+        return errScan
+    }
+    c.AddOption("variables", "email", string(bytes.TrimRight(buff, "\n")))
+    var (
+        repoName string
+        repoOk   bool
+    )
+    for !repoOk {
+        fmt.Printf("Enter a repository type ('git', or none): ")
+        buff, _, errScan = scanner.ReadLine()
+        if errScan != nil {
+            return errScan
+        }
+        repoName = string(bytes.TrimRight(buff, "\n"))
+        switch repoName {
+        case "":
+            fallthrough
+        case "git":
+            repoOk = true
+        default:
+            fmt.Printf("I didn't understand repo type %s\n", repoName)
+        }
+    }
+    c.AddOption("general", "repo", repoName)
+    var (
+        hostName string
+        hostOk   bool
+    )
+    for !hostOk {
+        fmt.Printf("Enter a repo host ('github', or none): ")
+        buff, _, errScan = scanner.ReadLine()
+        if errScan != nil {
+            return errScan
+        }
+        hostName = string(bytes.TrimRight(buff, "\n"))
+        switch hostName {
+        case "":
+            fallthrough
+        case "github":
+            hostOk = true
+        default:
+            fmt.Printf("I didn't understand repo host %s\n", hostName)
+        }
+    }
+    c.AddOption("general", "host", hostName)
+    var hostuser = ""
+    var hostuserOk = hostName != "github"
+    for !hostuserOk {
+        fmt.Printf("Enter a %s username: ", hostName)
+        buff, _, errScan = scanner.ReadLine()
+        if errScan != nil {
+            return errScan
+        }
+        hostuser = string(bytes.TrimRight(buff, "\n"))
+        if hostuser == "" {
+            fmt.Printf("Invalid %s username '%s'\n", hostName, hostuser)
+        } else {
+            hostuserOk = true
+        }
+    }
+    c.AddOption("general", "hostuser", hostuser)
+    return c.WriteFile(ConfigFilename, FilePermissions, "Generated configuration for gonew.")
 }
