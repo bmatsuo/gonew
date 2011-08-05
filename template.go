f94bf4598b355be7790efe5e7a4466a37bd58d0f
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-08-05 04:43:26 -0700

    Clean up template.go

--- template.go
+++ template.go
@@ -37,7 +37,6 @@ func TestName(filename string) string {
     return strings.Title(test)
 }
 
-
 //  The $GOROOT environment variable.
 func GetGoroot() string {
     goroot, err := os.Getenverror("GOROOT")
@@ -49,9 +48,8 @@ func GetGoroot() string {
 
 //  The template directory of the goinstall'ed gonew package.
 func GetTemplateRoot() []string {
-    var goroot = GetGoroot()
-    return []string{goroot, "src", "pkg",
-            "github.com", "bmatsuo", "gonew", "templates"}
+    return []string{GetGoroot(), "src", "pkg",
+        "github.com", "bmatsuo", "gonew", "templates"}
 }
 
 //  Get a full template path from a path slice relative to the templates
@@ -59,19 +57,17 @@ func GetTemplateRoot() []string {
 func GetTemplatePath(relpath []string) string {
     var (
         rootpath = GetTemplateRoot()
-        path = make([]string, len(rootpath)+len(relpath))
+        path     = make([]string, len(rootpath)+len(relpath))
     )
     copy(path, rootpath)
     copy(path[len(rootpath):], relpath)
-    var (
-        joined = filepath.Join(path...)
-        stat, errStat = os.Stat(joined)
-    )
-    if stat == nil || errStat != nil {
+    joined := filepath.Join(path...)
+    if stat, err := os.Stat(joined); stat == nil || err != nil {
         return ""
     }
     return joined
 }
+
 func GetAltTemplatePath(relpath []string) string {
     if AppConfig.AltRoot == "" {
         if DEBUG {
@@ -79,11 +75,8 @@ func GetAltTemplatePath(relpath []string) string {
         }
         return ""
     }
-    var (
-        altpath = GetRootedTemplatePath([]string{AppConfig.AltRoot}, relpath)
-        stat, errStat = os.Stat(altpath)
-    )
-    if stat == nil || errStat != nil {
+    altpath := GetRootedTemplatePath([]string{AppConfig.AltRoot}, relpath)
+    if stat, err := os.Stat(altpath); stat == nil || err != nil {
         if DEBUG {
             log.Printf("Error stat'ing %s.", altpath)
         }
@@ -91,134 +84,137 @@ func GetAltTemplatePath(relpath []string) string {
     }
     return altpath
 }
+
 //  Get a full template path from a path slice relative to another path
 //  slice.
 func GetRootedTemplatePath(rootpath []string, relpath []string) string {
-    var path = make([]string, len(rootpath)+len(relpath))
+    path := make([]string, len(rootpath)+len(relpath))
     copy(path, rootpath)
     copy(path[len(rootpath):], relpath)
     return filepath.Join(path...)
 }
+
 func extraData(filename string) map[string]string {
-    return map[string]string{"file":filename, "test":TestName(filename)}
+    return map[string]string{"file": filename, "test": TestName(filename)}
 }
-func combinedData(dict, extra map[string]string) map[string]string {
-    var numEntries = len(dict)+len(extra)
-    var combined = make(map[string]string, numEntries)
-    for k, v := range dict {
-        combined[k] = v
-    }
-    for k, v := range extra {
-        combined[k] = v
-    }
-    return combined
-}
-func ParseAltTemplate(filename string, dict map[string]string, relpath []string) (string, os.Error) {
-    var tpath = GetAltTemplatePath(relpath)
-    if tpath == "" {
-        return "", NoTemplateError
-    }
-    if DEBUG && DEBUG_LEVEL > 0 {
-        log.Printf("scanning: %s", tpath)
-        if DEBUG_LEVEL > 1 {
-            log.Printf("context:\n%v", dict)
+
+func combined(dict, extra map[string]string) map[string]string {
+    numEntries := len(dict) + len(extra)
+    comb := make(map[string]string, numEntries)
+    add := func(d map[string]string) {
+        for k, v := range d {
+            comb[k] = v
         }
     }
-    var template = template.MustParseFile(tpath, nil)
-    var buff = bytes.NewBuffer(make([]byte, 0, 1<<20))
-    var errTExec = template.Execute(buff, combinedData(dict, extraData(filename)))
-    return buff.String(), errTExec
-    //return mustache.RenderFile(tpath, dict, map[string]string{"file":filename, "test":TestName(filename)}), nil
+    add(dict)
+    add(extra)
+    return comb
 }
+
+func ParseAltTemplate(filename string, dict map[string]string, relpath []string) (string, os.Error) {
+    var templ *template.Template
+    if tpath := GetTemplatePath(relpath); tpath == "" {
+        return "", NoTemplateError
+    } else {
+        templ = template.MustParseFile(tpath, nil)
+        if DEBUG && DEBUG_LEVEL > 0 {
+            log.Printf("scanning: %s", tpath)
+            if DEBUG_LEVEL > 1 {
+                log.Printf("context:\n%v", dict)
+            }
+        }
+    }
+
+    buff := bytes.NewBuffer(make([]byte, 0, 1<<20))
+    errTExec := template.Execute(buff, combined(dict, extraData(filename)))
+    return buff.String(), errTExec
+}
+
 //  Given a filename and dictionary context, create a context dict+("file"=>filename),
 //  and read a template specified by relpath. See GetTemplatePath().
 func ParseTemplate(filename string, dict map[string]string, relpath []string) (string, os.Error) {
-    var tpath = GetTemplatePath(relpath)
-    if tpath == "" {
+    var templ *template.Template
+    if tpath := GetTemplatePath(relpath); tpath == "" {
         return "", NoTemplateError
-    }
-    if DEBUG && DEBUG_LEVEL > 0 {
-        log.Printf("scanning: %s", tpath)
-        if DEBUG_LEVEL > 1 {
-            log.Printf("context:\n%v", dict)
+    } else {
+        templ = template.MustParseFile(tpath, nil)
+
+        if DEBUG && DEBUG_LEVEL > 0 {
+            log.Printf("scanning: %s", tpath)
+            if DEBUG_LEVEL > 1 {
+                log.Printf("context:\n%v", dict)
+            }
         }
     }
-    var template = template.MustParseFile(tpath, nil)
-    var buff = bytes.NewBuffer(make([]byte, 0, 1<<20))
-    var errTExec = template.Execute(buff, combinedData(dict, extraData(filename)))
+
+    buff := bytes.NewBuffer(make([]byte, 0, 1<<20))
+    errTExec := template.Execute(buff, combined(dict, extraData(filename)))
     return buff.String(), errTExec
-    //return mustache.RenderFile(tpath, dict, map[string]string{"file":filename, "test":TestName(filename)}), nil
 }
+
 //  Given a filename, dictionary context, and the path to a template,
 //  write the parsed template to the specified filename. The context of
 //  the template will have a rule "file":filename which should override
 //  any previous "file" rule in dict.
-func WriteTemplate(filename, desc string, dict map[string]string, relpath...string) os.Error {
-    var template string
-    var alttemplate, errParseAlt = ParseAltTemplate(filename, dict, relpath)
-    if errParseAlt == nil {
-        template = alttemplate
+func WriteTemplate(filename, desc string, dict map[string]string, relpath ...string) os.Error {
+    var templ string
+    if altt, err := ParseAltTemplate(filename, dict, relpath); err == nil {
+        templ = altt
         if DEBUG || VERBOSE {
             fmt.Printf("Using alternate template %s\n", GetAltTemplatePath(relpath))
         }
+    } else if stdt, err := ParseTemplate(filename, dict, relpath); err == nil {
+        templ = stdt
     } else {
-        var stdtemplate, errParseStd = ParseTemplate(filename, dict, relpath)
-        if errParseStd != nil {
-            return errParseStd
-        }
-        template = stdtemplate
+        return err
     }
-	if DEBUG || VERBOSE {
-		fmt.Printf("Creating %s %s\n", desc, filename)
+
+    if DEBUG || VERBOSE {
+        fmt.Printf("Creating %s %s\n", desc, filename)
         if DEBUG && DEBUG_LEVEL > 2 {
             log.Print("\n", template, "\n")
         }
     }
-    var templout = make([]byte, len(template))
+
+    templout := make([]byte, len(template))
     copy(templout, template)
-    var errWrite = ioutil.WriteFile(filename, templout, FilePermissions)
-    return errWrite
+    return ioutil.WriteFile(filename, templout, FilePermissions)
 }
-func AppendTemplate(filename, desc string, dict map[string]string, relpath...string) os.Error {
-    var template string
-    var alttemplate, errParseAlt = ParseAltTemplate(filename, dict, relpath)
-    if errParseAlt == nil {
-        template = alttemplate
+func AppendTemplate(filename, desc string, dict map[string]string, relpath ...string) os.Error {
+    var templ string
+    if altt, err := ParseAltTemplate(filename, dict, relpath); err == nil {
+        templ = altt
         if DEBUG || VERBOSE {
             fmt.Printf("Using alternate template %s\n", GetAltTemplatePath(relpath))
         }
+    } else if stdt, err := ParseTemplate(filename, dict, relpath); err == nil {
+        templ = stdt
     } else {
-        var stdtemplate, errParseStd = ParseTemplate(filename, dict, relpath)
-        if errParseStd != nil {
-            return errParseStd
-        }
-        template = stdtemplate
+        return err
     }
-	if DEBUG || VERBOSE {
-		fmt.Printf("Appending %s %s\n", desc, filename)
+
+    if DEBUG || VERBOSE {
+        fmt.Printf("Appending %s %s\n", desc, filename)
         if DEBUG && DEBUG_LEVEL > 2 {
-            log.Print("\n", template, "\n")
+            log.Print("\n", templ, "\n")
         }
     }
-    var fout, errOpen = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, FilePermissions)
-    if errOpen != nil {
-        return errOpen
+
+    fout, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, FilePermissions)
+    if err != nil {
+        return err
     }
-    var _, errAppend = fout.WriteString(template)
-    if errAppend != nil {
-        return errAppend
+    if _, err := fout.WriteString(templ); err != nil {
+        return err
     }
-    var errClose = fout.Close()
-    if errClose != nil {
-        return errClose
+    if err := fout.Close(); err != nil {
+        return err
     }
     return nil
 }
 
 /* Some functions for tests and debugging. */
-func getDebugTemplateRoot() []string {
-    return []string{"templates"}
-}
-func getDebugTemplatePath(relpath...string) string {
+func getDebugTemplateRoot() []string { return []string{"templates"} }
+func getDebugTemplatePath(relpath ...string) string {
     return GetRootedTemplatePath(getDebugTemplateRoot(), relpath)
 }
