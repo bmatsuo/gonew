255b96046ba3a8663ff2b8c0f275aa5b6ee678e3
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-10-22 17:57:39 -0700

    Add some unused code prepping for cleaner template execution.

--- template.go
+++ template.go
@@ -10,6 +10,7 @@ package main
  *  Description: 
  */
 import (
+    "io"
     "os"
     "fmt"
     //"log"
@@ -21,10 +22,83 @@ import (
     //"github.com/hoisie/mustache.go"
 )
 
+const (
+    TemplateFileExt = ".t"
+)
+
 var (
     NoTemplateError = os.NewError("Requested template does not exist")
     ParseError      = os.NewError("Couldn't parse template")
 )
+
+type Executor interface {
+    Execute(io.Writer, interface{}) os.Error
+}
+
+type ExecutorSet interface {
+    Execute(io.Writer, string, interface{}) os.Error
+}
+
+func collectBytes(f func(w io.Writer) os.Error) (p []byte, err os.Error) {
+    b := new(bytes.Buffer)
+    err = f(b)
+    p = b.Bytes()
+    return
+}
+
+//  Call t.Execute with the given data, writing to a []byte buffer.
+func Executed(t Executor, data interface{}) ([]byte, os.Error) {
+    return collectBytes(func(w io.Writer)os.Error { return t.Execute(w, data) })
+}
+
+//  Call s.Execute with the given name data, writing to a []byte buffer.
+func ExecutedSet(s ExecutorSet, name string, data interface{}) ([]byte, os.Error) {
+    return collectBytes(func(w io.Writer)os.Error { return s.Execute(w, name, data) })
+}
+
+//  Returns an os-specific pattern <root>/"*"/*<TemplateFileExt>
+func TemplateGlobPattern(root string) string {
+    return filepath.Join(root, "*", fmt.Sprintf("*%s", TemplateFileExt))
+}
+
+type TemplateMultiSet []*template.Set
+
+//  Call function CollectTemplates on each given root and create a TemplateMultiSet.
+func MakeTemplateMultiSet(f template.FuncMap, roots... string) (ms TemplateMultiSet, err os.Error) {
+    var s *template.Set
+    ms = make(TemplateMultiSet, len(roots))
+    for i := range roots {
+        if s, err = CollectTemplates(roots[i], f); err != nil {
+            break
+        } else {
+            ms[i] = s
+        }
+    }
+    return
+}
+
+//  Execute a template in first s in ms for which s.Template(name) is non-nil.
+func (ms TemplateMultiSet) Execute(wr io.Writer, name string, data interface{}) os.Error {
+    for _, s := range ms {
+        if t := s.Template(name); t != nil {
+            return t.Execute(wr, data)
+        }
+    }
+    return NoTemplateError
+}
+
+//  Parse templates <root>/*/*.t, allowing them a given function map.
+func CollectTemplates(root string, f template.FuncMap) (s *template.Set, err os.Error) {
+    s = new(template.Set)
+    if f != nil {
+        s.Funcs(f)
+    }
+    s, err = s.ParseTemplateGlob(TemplateGlobPattern(root))
+    if err != nil {
+        return
+    }
+    return
+}
 
 func TestName(filename string) string {
     var test = filename
