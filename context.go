fb1f026d572eb65c4ba980c1e1b59d667498ec6d
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-11-05 19:33:04 -0700

    Gofix to correct os.Error errors. Results UNCHECKED.

--- context.go
+++ context.go
@@ -11,8 +11,8 @@ package main
  */
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 //  A WriteMode determines how a Context should be written.
@@ -20,120 +20,120 @@ type WriteMode uint
 
 //  A Context can either be written by being appended to a file, or creating
 //  the file and truncating the file.
-const(
-    AppendMode WriteMode = iota
-    CreateMode
+const (
+	AppendMode WriteMode = iota
+	CreateMode
 )
 
 var writeModeStrings = []string{
-    AppendMode:"Appending",
-    CreateMode:"Creating",
+	AppendMode: "Appending",
+	CreateMode: "Creating",
 }
+
 func (wm WriteMode) String() string { return writeModeStrings[wm] }
 
 //  Write a Context to the file it specifies using a specified WriteMode.
 //  WriteContext uses template named tname in the templates ExecutorSet. The
 //  value desc is only used for printing debugging information.
-func WriteContext(context Context, mode WriteMode, templates ExecutorSet, tname, desc string) os.Error {
-    filename := context.Filename()
-    Verbose(fmt.Sprintf("WriteContext: %s %s %s\n", mode.String(), desc, filename))
-    Debug(1, fmt.Sprintf("Executing template %s", tname))
+func WriteContext(context Context, mode WriteMode, templates ExecutorSet, tname, desc string) error {
+	filename := context.Filename()
+	Verbose(fmt.Sprintf("WriteContext: %s %s %s\n", mode.String(), desc, filename))
+	Debug(1, fmt.Sprintf("Executing template %s", tname))
 
-    // Execute the template.
-    p, errExec := ExecutedSet(templates, tname, context)
-    if errExec != nil {
-        return errExec
-    }
-    Debug(2, fmt.Sprintf("\n%s\n", p))
+	// Execute the template.
+	p, errExec := ExecutedSet(templates, tname, context)
+	if errExec != nil {
+		return errExec
+	}
+	Debug(2, fmt.Sprintf("\n%s\n", p))
 
-    // Open the output file.
-    openMode := os.O_WRONLY
-    if mode == AppendMode {
-        openMode |= os.O_APPEND
-    } else {
-        openMode |= os.O_CREATE | os.O_TRUNC
-    }
-    fout, err := os.OpenFile(filename, openMode, FilePermissions)
-    if err != nil {
-        return err
-    }
+	// Open the output file.
+	openMode := os.O_WRONLY
+	if mode == AppendMode {
+		openMode |= os.O_APPEND
+	} else {
+		openMode |= os.O_CREATE | os.O_TRUNC
+	}
+	fout, err := os.OpenFile(filename, openMode, FilePermissions)
+	if err != nil {
+		return err
+	}
 
-    // Write out the executed template and close the file
-    if _, err := fout.Write(p); err != nil {
-        return err
-    }
-    if err := fout.Close(); err != nil {
-        return err
-    }
-    return nil
+	// Write out the executed template and close the file
+	if _, err := fout.Write(p); err != nil {
+		return err
+	}
+	if err := fout.Close(); err != nil {
+		return err
+	}
+	return nil
 }
 
 //  Write the file specified in c using the template tname from source ms.
-func CreateFile(c Context, tname string, ms TemplateMultiSet) (err os.Error) {
-    mainWriteMode := CreateMode
-    debugdesc := c.DebugDescription()
+func CreateFile(c Context, tname string, ms TemplateMultiSet) (err error) {
+	mainWriteMode := CreateMode
+	debugdesc := c.DebugDescription()
 
-    // Analyze license and file type.
-    ltype := c.LicenseType()
-    license := ltype.TemplateName(c.FileType())
-    pos := ltype.Position(c.FileType())
-    showLicense := pos != 0 && license != ""
+	// Analyze license and file type.
+	ltype := c.LicenseType()
+	license := ltype.TemplateName(c.FileType())
+	pos := ltype.Position(c.FileType())
+	showLicense := pos != 0 && license != ""
 
-    // Write out a license header for the file.
-    if showLicense && pos < 0 {
-        err = WriteContext(c, CreateMode, ms, license, debugdesc + " license")
-        if err != nil {
-            return
-        }
-        mainWriteMode = AppendMode
-    }
+	// Write out a license header for the file.
+	if showLicense && pos < 0 {
+		err = WriteContext(c, CreateMode, ms, license, debugdesc+" license")
+		if err != nil {
+			return
+		}
+		mainWriteMode = AppendMode
+	}
 
-    // Write the main file contents.
-    err = WriteContext(c, mainWriteMode, ms, tname, debugdesc)
-    if err != nil {
-        return
-    }
+	// Write the main file contents.
+	err = WriteContext(c, mainWriteMode, ms, tname, debugdesc)
+	if err != nil {
+		return
+	}
 
-    // Write out a license footer for the file.
-    if showLicense && pos > 0 {
-        err = WriteContext(c, AppendMode, ms, license, debugdesc + " license")
-        if err != nil {
-            return
-        }
-    }
+	// Write out a license footer for the file.
+	if showLicense && pos > 0 {
+		err = WriteContext(c, AppendMode, ms, license, debugdesc+" license")
+		if err != nil {
+			return
+		}
+	}
 
-    // TODO: check the new file into git under certain conditions...
+	// TODO: check the new file into git under certain conditions...
 
-    return
+	return
 }
 
 //  A Context values are used as the data for gonew templates. The methods
 //  can be called from a template with ".<method>". For example
 //      This file is '{{ .Filename }}'
 type Context interface {
-    // The filename to which the Context is written out to.
-    Filename() string
-    // The name of the go package the Context belongs to. For commands, this
-    // should return "main". For packages, the package/target name.
-    Package() string
-    // The names of packages that source (non-test, non-options) .go files
-    // should import.
-    Imports() []string
-    // The description of the Context. This should be unique for each file
-    // and should describe the file's purpose.
-    Description() string
-    // The names of tests for the Context. Really only used for generating
-    // test files
-    Tests() []string
-    // The FileType of the Context. This is used when generating license text.
-    FileType() FileType
-    // The LicenseType for generating file licenses.
-    LicenseType() LicenseType
-    // A description of the file for printing debug information. This is not
-    // very useful in templates.
-    DebugDescription() string
-    // The Project associated with the file. This is the zero Project for
-    // any library files (generated with the command gonew lib NAME PKG)
-    Project() Project
+	// The filename to which the Context is written out to.
+	Filename() string
+	// The name of the go package the Context belongs to. For commands, this
+	// should return "main". For packages, the package/target name.
+	Package() string
+	// The names of packages that source (non-test, non-options) .go files
+	// should import.
+	Imports() []string
+	// The description of the Context. This should be unique for each file
+	// and should describe the file's purpose.
+	Description() string
+	// The names of tests for the Context. Really only used for generating
+	// test files
+	Tests() []string
+	// The FileType of the Context. This is used when generating license text.
+	FileType() FileType
+	// The LicenseType for generating file licenses.
+	LicenseType() LicenseType
+	// A description of the file for printing debug information. This is not
+	// very useful in templates.
+	DebugDescription() string
+	// The Project associated with the file. This is the zero Project for
+	// any library files (generated with the command gonew lib NAME PKG)
+	Project() Project
 }
-
--- template.go
+++ template.go
@@ -10,99 +10,99 @@ package main
  *  Description: 
  */
 import (
-    "io"
-    "os"
-    "fmt"
-    //"log"
-    "bytes"
-    //"strings"
-    //"io/ioutil"
-    "path/filepath"
-    "template"
-    "runtime"
-    //"github.com/hoisie/mustache.go"
+	"errors"
+	"io"
+	"fmt"
+	//"log"
+	"bytes"
+	//"strings"
+	//"io/ioutil"
+	"path/filepath"
+	"template"
+	"runtime"
+	//"github.com/hoisie/mustache.go"
 )
 
 const (
-    TemplateFileExt = ".t"
+	TemplateFileExt = ".t"
 )
 
 var (
-    NoTemplateError = os.NewError("Requested template does not exist")
-    ParseError      = os.NewError("Couldn't parse template")
+	NoTemplateError = errors.New("Requested template does not exist")
+	ParseError      = errors.New("Couldn't parse template")
 )
 
 type Executor interface {
-    Execute(io.Writer, interface{}) os.Error
+	Execute(io.Writer, interface{}) error
 }
 
 type ExecutorSet interface {
-    Execute(io.Writer, string, interface{}) os.Error
+	Execute(io.Writer, string, interface{}) error
 }
 
-func collectBytes(f func(w io.Writer) os.Error) (p []byte, err os.Error) {
-    b := new(bytes.Buffer)
-    err = f(b)
-    p = b.Bytes()
-    return
+func collectBytes(f func(w io.Writer) error) (p []byte, err error) {
+	b := new(bytes.Buffer)
+	err = f(b)
+	p = b.Bytes()
+	return
 }
 
 //  Call t.Execute with the given data, writing to a []byte buffer.
-func Executed(t Executor, data interface{}) ([]byte, os.Error) {
-    return collectBytes(func(w io.Writer)os.Error { return t.Execute(w, data) })
+func Executed(t Executor, data interface{}) ([]byte, error) {
+	return collectBytes(func(w io.Writer) error { return t.Execute(w, data) })
 }
 
 //  Call s.Execute with the given name data, writing to a []byte buffer.
-func ExecutedSet(s ExecutorSet, name string, data interface{}) ([]byte, os.Error) {
-    return collectBytes(func(w io.Writer)os.Error { return s.Execute(w, name, data) })
+func ExecutedSet(s ExecutorSet, name string, data interface{}) ([]byte, error) {
+	return collectBytes(func(w io.Writer) error { return s.Execute(w, name, data) })
 }
 
 //  Returns an os-specific pattern <root>/"*"/*<TemplateFileExt>
 func TemplateGlobPattern(root string) string {
-    return filepath.Join(root, "*", fmt.Sprintf("*%s", TemplateFileExt))
+	return filepath.Join(root, "*", fmt.Sprintf("*%s", TemplateFileExt))
 }
 
 type TemplateMultiSet []*template.Set
 
 //  Call function CollectTemplates on each given root and create a TemplateMultiSet.
-func MakeTemplateMultiSet(f template.FuncMap, roots... string) (ms TemplateMultiSet, err os.Error) {
-    var s *template.Set
-    ms = make(TemplateMultiSet, len(roots))
-    for i := range roots {
-        if s, err = CollectTemplates(roots[i], f); err != nil {
-            break
-        } else {
-            ms[i] = s
-        }
-    }
-    return
+func MakeTemplateMultiSet(f template.FuncMap, roots ...string) (ms TemplateMultiSet, err error) {
+	var s *template.Set
+	ms = make(TemplateMultiSet, len(roots))
+	for i := range roots {
+		if s, err = CollectTemplates(roots[i], f); err != nil {
+			break
+		} else {
+			ms[i] = s
+		}
+	}
+	return
 }
 
 //  Execute a template in first s in ms for which s.Template(name) is non-nil.
-func (ms TemplateMultiSet) Execute(wr io.Writer, name string, data interface{}) os.Error {
-    for _, s := range ms {
-        if t := s.Template(name); t != nil {
-            return t.Execute(wr, data)
-        }
-    }
-    return NoTemplateError
+func (ms TemplateMultiSet) Execute(wr io.Writer, name string, data interface{}) error {
+	for _, s := range ms {
+		if t := s.Template(name); t != nil {
+			return t.Execute(wr, data)
+		}
+	}
+	return NoTemplateError
 }
 
 //  Parse templates <root>/*/*.t, allowing them a given function map.
-func CollectTemplates(root string, f template.FuncMap) (s *template.Set, err os.Error) {
-    s = new(template.Set)
-    if f != nil {
-        s.Funcs(f)
-    }
-    s, err = s.ParseTemplateGlob(TemplateGlobPattern(root))
-    if err != nil {
-        return
-    }
-    return
+func CollectTemplates(root string, f template.FuncMap) (s *template.Set, err error) {
+	s = new(template.Set)
+	if f != nil {
+		s.Funcs(f)
+	}
+	s, err = s.ParseTemplateGlob(TemplateGlobPattern(root))
+	if err != nil {
+		return
+	}
+	return
 }
 
 //  The template directory of the goinstall'ed gonew package.
 func GetTemplateRoot() []string {
-    return []string{runtime.GOROOT(), "src", "pkg",
-        "github.com", "bmatsuo", "gonew", "templates"}
+	return []string{runtime.GOROOT(), "src", "pkg",
+		"github.com", "bmatsuo", "gonew", "templates"}
 }
