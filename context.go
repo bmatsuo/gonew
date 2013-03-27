2a53dd912189689f32a57904439fe8452df70789
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-03-27 00:50:49 -0700

    [docs] clean up the context interface

--- context.go
+++ context.go
@@ -31,32 +31,30 @@ func CreateFile(c Context, tname string) (err error) {
 	return
 }
 
-//  A Context values are used as the data for gonew templates. The methods
-//  can be called from a template with ".<method>". For example
-//      This file is '{{ .Filename }}'
+// A file context. Its values provide data for gonew templates.
 type Context interface {
-	// The filename to which the Context is written out to.
-	Filename() string
-	// The name of the go package the Context belongs to. For commands, this
-	// should return "main". For packages, the package/target name.
-	Package() string
-	// The names of packages that source (non-test, non-options) .go files
-	// should import.
-	Imports() []string
-	// The description of the Context. This should be unique for each file
-	// and should describe the file's purpose.
-	Description() string
-	// The names of tests for the Context. Really only used for generating
-	// test files
-	Tests() []string
-	// The FileType of the Context. This is used when generating license text.
+	// The file's type.
 	FileType() FileType
+	// The filename being written out to.
+	Filename() string
+	// The package (basename) the file belongs to.
+	Package() string
+
+	// The Project associated with the file.
+	Project() Project
+
+	// The file's description for documentation.
+	Description() string
+	// The names of test functions. Should only be used for generating test files.
+	Tests() []string
+
+	// TODO this is probably not necessary.
 	// The LicenseType for generating file licenses.
 	LicenseType() LicenseType
-	// A description of the file for printing debug information. This is not
-	// very useful in templates.
+	// TODO this is probably not necessary
+	// Packages source files (non-test, non-options) should import.
+	Imports() []string
+
+	// A description of the context for printing debug information. Not useful in templates.
 	DebugDescription() string
-	// The Project associated with the file. This is the zero Project for
-	// any library files (generated with the command gonew lib NAME PKG)
-	Project() Project
 }
