4119a520bc71558ae56c9cb2f2b8b6633873440c
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-04-14 01:19:07 -0700

    [docs] godoc update for v2

--- doc.go
+++ doc.go
@@ -3,162 +3,60 @@
 // license that can be found in the LICENSE file.
 
 /*
-Gonew generates new Go project directories. Its produced project
-directories contain stub files and initialized repositories (only
-git/github supported now). It can be used to create new packages and
-commands.
-
-The gonew configuration file is stored at ~/.gonewrc. It is generated the
-first time you run gonew. Command line options can be used to override
-some details of the configuration file.
+Gonew generates new Go projects. The produced projects contain stub files and
+can optionally initialize repositories and add files to them.
 
 Usage:
 
-    gonew [options] cmd NAME
-    gonew [options] pkg NAME
-    gonew [options] lib NAME PKG
+    gonew [options] project targec
 
 Arguments:
 
-    NAME
-            The name of the new project/repo.
-
-    PKG
-            The package a library (.go file) belongs to.
+	project: The type of project to generate
+	target: The name from which filenames are based
 
 Options:
 
-	-test=false
-			Test files will not be produced.
-
-    -target=""
-            The executable name in case the argument TYPE is "cmd". The
-			package name when TYPE is "pkg". The default value based on
-			the argument NAME.
-
-    -import=""
-            Colon ':' separated list of packages to include in source
-            .go files. The packages are not imported in any tests, or
-            the options.go file (or doc.go) created for cmd projects.
-
-    -repo=""
-            Repository type (currently, "git" is the only supported
-            repository type).
-
-    -remote=""
-            When passed a url to a remote repository, attempt to
-            initialize the remote repository to the new project's
-            repository. The url passed in must agree with the host
-            specified in the config file (or by -host).
-
-    -host=""
-            Repository host if any (currently, "github" is the only
-            supported host). The value supplied must agree with the
-            value of -repo.
-
-    -user=""
-            Username for the repo host (necessary for "github").
-
-    -v
-            Print verbose output to the stdout (this intersects with
-            some -debug output).
-
-    -debug=-1
-            When set to a non-negative value, debugging output will be
-            printed.
-
-    -help
-            Print a usage message
-
-
-Configuration:
-
-The configuration for gonew is simple. The configuration can provide
-default hosts, usernames, and repositories. However, it also contains the
-setting of the {{name}} and {{email}} template function values.
-
-The configuration file for gonew (~/.gonewrc) is generated on the spot if
-one does not exist. So you do not need to worry about editing it for the
-most part.
-
-If you wish to write/edit your own configuration file. An example can be
-found at the path
-
-    $GOROOT/src/pkg/github.com/bmatsuo/gonew/gonewrc.example
+	-config="": specify config path
+	-env="": specify a user environment
+	-pkg="": specify a package name
 
 Examples:
 
-    gonew -target=mp3lib pkg go-mp3lib
-    gonew lib decode mp3lib
-    gonew -remote=git@github.com:bmatsuo/goplay.git cmd goplay
+    gonew pkg go-mp3lib
+    gonew -pkg mp3lib lib decode
+    gonew cmdtest goplay
+
+Configuration:
+
+Gonew is configured via a JSON file stored in ~/.config/gonew.json. An example
+can be found in gonew.json.example The configuration file specifies
+environments, projects, and the locations of externally defined templates. An
+environement hold information used in template rendering like user metadata and
+import paths for created projects. A project configuration describes the files
+contained in a project and script hooks to execute on file creation.
+Environments can inherit/override other environments and projects can
+inherit/override from other projects.
 
 Custom Templates:
 
-Custom file templates can be created and stored in a heirarchy separate
-from the standard Gonew template heirarchy at
-
-    $GOROOT/src/pkg/github.com/bmatsuo/gonew/templates
-
-Supply a custom template heirarchy path in the ~/.gonewrc file. See the
-example config file for more info about how to do this.
-
-The custom heirarchy is parsed as a template.Set and can reference other
-templates in the heirarchy (http://golang.org/pkg/template). Templates,
-however, cannot reference templates in the default Gonew template
-heirarchy. In order to do so, copies or symbolic links of the desired
-templates must be made in the custom heirarchy.
-
-Templates must be given a ".t" extension in order to be recognized and
-parsed by Gonew. When generating a new project or library, Gonew uses
-templates with specific names to generate the files. In order to use
-custom templates, a template with the proper name must be found in the
-custom heirarchy. Increase the Gonew debugging variable for information
-about with templates are being executed.
+Users can define their own set of custom templates. This is done by adding
+entries to the ExternalTemplates array in the configuration file. Templates
+can make use of the standard gonew templates (in the "templates" directory).
+Templates must have the .t2 file extension to be recognized by Gonew.
 
 Template Functions:
 
-All templates used by Gonew have acces to a small library of simple
-helper functions. The {{name}} and {{email}} variables have already been
-discussed. Here is list of all available template functions.
+Templates in Gonew have acces to a small library of helper functions Here is
+list of all available template functions.
 
-    import [PACKAGE ...]
-            Produces an import statement which includes the packages
-            specified in it arguments. The arguments can be either
-            strings or slices of strings.
-
-    func NAME [ARGUMENT ...]
-            Produces the definition of a function with zero return
-            values. The arguments should be supplied as identifier-type
-            pairs. The name should be a valid go identifier.
-
-	main [EXPRESSION ...]
-			Produces a command's func main() containing the provided
-			list of expressions (one expression per string), one per line.
-
-	init [EXPRESSION ...]
-			Like the function 'main', but produces a func init().
-
-    date [FORMAT]
-            Produces the current date-time as human readable string when
-            called with no argument. A format can be supplied in the form
-            of an example string formatting of a specific day (see "time").
-
-    year
-            Produces the current year in a four-digit format.
-
-    name
-            Produces the Gonew user's name as defined in ~/.gonewrc
-
-    email
-            Produces the Gonew user's email as defined in ~/.gonewrc
-
-Template Contexts:
-
-The data supplied to each template a type known as a Context. Contexts
-have a defined method interface that can be called from inside a template.
-Read about the Context interface using godoc
-
-    godoc $GOROOT/src/pkg/github.com/bmatsuo/gonew Context | less
+		name: the user's name specified in the environment
+		email: the user's email specified in the environment
+		year: the year in 4-digit format
+		time: the time with an optional format string argument
+		date: the date with an optional format string argument
+		import: import an arbitrary number of packages into go source files
+		equal: compare two values for equality
 
 */
 package documentation
